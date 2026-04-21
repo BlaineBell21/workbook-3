db492d303f1d7cb1cf2c2792b1fcaccf8d865259
Blaine Bell <blainebell21@gmail.com>
2026-04-20 17:04:28 -0700

    exercise 3 - pg 17 Payroll Calculator

---
+++ src/main/java/com/pluralsight/Reader.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+    public static void fileReader(String[] args){
+
+
+    }
+}
